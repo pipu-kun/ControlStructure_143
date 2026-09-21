fbe8f9f351a49f63d81b480ae50c9b081f253632
pipu-kun <SideActivity@proton.me>
2026-09-21 10:58:39 +0700

    renamed file

---
+++ Soal1.py
@@ -0,0 +1,12 @@
+StudentPerformance = float(input("here: "))
+
+if StudentPerformance >= 90:
+    print("Excellent")
+elif StudentPerformance >= 80:
+    print("Very Good")
+elif StudentPerformance >= 70:
+    print("Good")
+elif StudentPerformance >= 60:
+    print("Average")
+else:
+    print("Below Average")
